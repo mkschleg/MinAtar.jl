880490987a4bfeff569bfd21ccc7b5c16dfa99f7
Matthew Schlegel <mkschleg@gmail.com>
2020-02-13 21:57:49 -0700

    Fix build?

--- deps/build.jl
+++ deps/build.jl
@@ -1,18 +1,23 @@
 
 
-# using Pkg; Pkg.install("Conda");
 using Conda
 using PyCall
 
-if findfirst("pip", read(`$(Conda.conda) list`, String)) == nothing
-    Conda.add("pip")
-    Conda.add("git")
-end
-
-if findfirst("minatar", read(`$(Conda.conda) list`, String)) == nothing
+if findfirst("minatar", read(`$pycmd -m pip list`)) == nothing
     pycmd = PyCall.python
+    run(`$pycmd -m pip install git`)
     run(`$pycmd -m pip install git+https://github.com/kenjyoung/MinAtar.git`)
 end
+
+# if findfirst("pip", read(`$(Conda.conda) list`, String)) == nothing
+#     Conda.add("pip")
+#     Conda.add("git")
+# end
+
+# if findfirst("minatar", read(`$(Conda.conda) list`, String)) == nothing
+#     pycmd = PyCall.python
+#     run(`$pycmd -m pip install git+https://github.com/kenjyoung/MinAtar.git`)
+# end
 # end
 
 
