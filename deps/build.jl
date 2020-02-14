18d2fce4a757326c68f2f450e08c210c7ecbf469
Matthew Schlegel <mkschleg@gmail.com>
2020-02-13 22:18:23 -0700

    -.- fixed??

--- deps/build.jl
+++ deps/build.jl
@@ -3,22 +3,25 @@
 using Conda
 using PyCall
 
-pycmd = PyCall.python
-if findfirst("minatar", read(`$pycmd -m pip list`)) == nothing
+
+if isfile(Conda.conda)
+    if findfirst("pip", read(`$(Conda.conda) list`, String)) == nothing
+        Conda.add("pip")
+        Conda.add("git")
+    end
+    
+    if findfirst("minatar", read(`$(Conda.conda) list`, String)) == nothing
+        pycmd = PyCall.python
+        run(`$pycmd -m pip install git+https://github.com/kenjyoung/MinAtar.git`)
+    end
+else
+    Conda.add("pip")
+    Conda.add("git")
     pycmd = PyCall.python
-    run(`$pycmd -m pip install git`)
     run(`$pycmd -m pip install git+https://github.com/kenjyoung/MinAtar.git`)
 end
 
-# if findfirst("pip", read(`$(Conda.conda) list`, String)) == nothing
-#     Conda.add("pip")
-#     Conda.add("git")
-# end
 
-# if findfirst("minatar", read(`$(Conda.conda) list`, String)) == nothing
-#     pycmd = PyCall.python
-#     run(`$pycmd -m pip install git+https://github.com/kenjyoung/MinAtar.git`)
-# end
 # end
 
 
